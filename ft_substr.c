276b17a8c9d1bd236b5735bb5151db10128eb876
decabral <deborahcabralsilva@gmail.com>
2025-11-04 19:11:53 -0300

    adds

--- ft_substr.c
+++ ft_substr.c
@@ -1,28 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: decabral <decabral@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/03 17:09:56 by decabral          #+#    #+#             */
-/*   Updated: 2025/11/03 17:10:45 by decabral         ###   ########.fr       */
+/*   Created: 2025/11/04 15:50:47 by decabral          #+#    #+#             */
+/*   Updated: 2025/11/04 15:57:41 by decabral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t	len;
-	char	*new;	
+	unsigned int	i;
+	size_t			length;
+	char			*new;
 
-	len = ft_strlen(s);
-	if (!(s))
+	i = 0;
+	length = ft_strlen(s) - start;
+	new = (char *) malloc((sizeof (char)) * length + 1);
+	if (!new || !s)
 		return (NULL);
-	new = (char *) malloc(len + 1);
-	if (new == NULL)
+	if (start >= len)
 		return (NULL);
-	ft_strlcpy(new, s, ft_strlen(s) + 1);
+	while (i < length)
+	{
+		new[i] = s[start + i];
+		i++;
+	}
 	return (new);
 }
+/*#include <stdio.h>
+int main (void)
+{
+	const char	*src = "dogcat";
+	printf("%s\n", ft_substr(src, 3, 6));
+}*/
